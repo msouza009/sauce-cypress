2a6092d6dc6ca85b2a8182263718427c992d97f0
Matheus <szmatheussouza@gmail.com>
2025-06-30 12:00:26 -0300

    novos casos de testes para login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,6 +7,46 @@ describe('Efetuando Login', () => {
     cy.contains('Products').should('be.visible')
   })
 
+  it.only('Login com usuário bloqueado', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('locked_out_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('contain', 'Epic sadface: Sorry, this user has been locked out.')
+  })
+
+  it.only('Login com usuário problem_user', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('problem_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.contains('Products').should('be.visible')
+  })
+
+  it.only('Login com usuário de performance', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('performance_glitch_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.contains('Products').should('be.visible')
+  })
+
+  it.only('Login com usuário error', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('error_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.contains('Products').should('be.visible')
+  })
+
+  it.only('Login com usuário visual', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('visual_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.contains('Products').should('be.visible')
+  })
+
   it.only('Login com usuário inválido', () => {
     cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('user_invalido')
